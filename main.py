ec1b4b9ba7bb15228657fa96b505bac3b4b4fa4e
estebanneuu <esteban@studioarverne.com>
2022-11-16 15:18:08 +0100

    First looking good commit

--- main.py
+++ main.py
@@ -6,42 +6,38 @@ import requests as rq
 from bs4 import BeautifulSoup as bs
 import re
 from dataclasses import dataclass
+
+
 @dataclass
 class Video:
-    id : int
-    title : str
-    author : str
-    description : str
+    id: int
+    title: str
+    author: str
+    description: str
 
-def write_json(table,outputname):
-    with open(outputname,'w') as ou:
-        json.dump(table,ou)
+
+def write_json(table, outputname):
+    with open(outputname, 'w') as ou:
+        json.dump(table, ou)
+
+
 def read_json(filename):
-    with open(filename,'r') as input:
-        return 1
-
-def parse_vide(video_id):
-    url = "https://www.youtube.com/watch?v="+video_id
-    soup = bs(rq.get(url).text,"html.parser")
+    with open(filename, 'r') as f:
+        return json.load(f)["video_id"]
 
 
-url = "https://www.youtube.com/watch?v=JwSCjUaa3DU"
-source = requests.get(url).text
-soup = bs(source, 'html.parser')
-div_s = soup.findAll('div')
-span_s = soup.findAll('span')
-Title = div_s[0].find("meta",{"itemprop":"name"}).get("content")
-Author = div_s[0].find("link",{"itemprop":"name"}).get("content")
-description = re.compile('(?<=shortDescription":").*(?=","isCrawlable)').findall(str(soup))[0].replace('\\n','\n')
-#likes = soup.find("div", class_="cbox yt-spec-button-shape-next--button-text-content").span.button.text
-likes = source[:source.find(' aime"')]
-var = likes[likes.rfind('"') + 1:]
-print(var)
 
-#print(likes)
-#print(description)
-print(Title)
-print(Author)
-with open('out.txt', 'w') as f:
-    with redirect_stdout(f):
-        print(soup)
+def parse_video(video_id):
+    video_url = "https://www.youtube.com/watch?v=" + video_id
+    soup = bs(rq.get(video_url).text, "html.parser")
+    div_s = soup.findAll('div')
+    video_title = div_s[0].find("meta", {"itemprop": "name"}).get("content")
+    video_author = div_s[0].find("link", {"itemprop": "name"}).get("content")
+    video_description = re.compile('(?<=shortDescription":").*(?=","isCrawlable)').findall(str(soup))[0].replace('\\n',
+                                                                                                                 '\n')
+    return Video(id=video_id, title=video_title, author=video_author, description=video_description)
+
+
+videos = read_json("input.json")
+for i in videos:
+    print(parse_video(i))
